f640fbceb3b1ec44ecad48b2f4f8385a415ba387
AKASH RAJ <akashraj9828@gmail.com>
2018-12-29 01:11:06 +0530

    finished.

--- a.java
+++ a.java
@@ -6,31 +6,44 @@ import java.util.*;
 class a {
     static Vector<String> header = new Vector<String>();
     static Boolean print_csv = false;
-    static Boolean print_header =true;
-    static String table_name = "airports";
-    static String csv_name = "temp.csv";    //input file
-    static String sql_name = "oupqow.sql";    //output file
-    
+    static Boolean print_header = true;
+    static Boolean verbose = true;
+    static Boolean ignoreError = false;
+    static String table_name = "TABLE_NAME";
+    static String csv_name = "in.csv"; // input file
+    static String sql_name = "out.sql"; // output file
 
-    /*args
-        [0]=input file.csv
-        [1]=output file.sql
-        [2]=table name
-        [3]=other args like show header or to make update sql
-        
-    */ 
+    /*
+     * args [0]=input file.csv [1]=output file.sql [2]=table name [3]=other args
+     * like show header or to make update sql
+     * 
+     */
     public static void main(String args[]) {
-        
-        if(args.length>0)
-        init(args);
+
+        if (args.length > 0)
+            init(args);
+        print_info();
         setHeaders();
         writeSQL();
     }
-    public static void init(String args[]){
-        csv_name=args[0];
-        sql_name=args[1];
-        table_name=args[2];
+
+    public static void init(String args[]) {
+        csv_name = args[0];
+        sql_name = args[1];
+        table_name = args[2];
     }
+
+    public static void print_info() {
+        print("^^^^^^^^^^^^^^^^^^^^^^^^");
+        print("Input file:- " + csv_name);
+        print("Output file:- " + sql_name);
+        print("Table name:- " + table_name);
+        print("Ignore error:- "+ignoreError);
+        print("Print header:- "+print_header);
+        print("^^^^^^^^^^^^^^^^^^^^^^^^");
+
+    }
+
     public static void setHeaders() {
         String s;
 
@@ -41,8 +54,9 @@ class a {
                 header.add(st.nextToken());
             }
 
-            if(print_header)
-            print("****************** HEADERS ******************\n"+ header.toString());
+            if (print_header)
+                print("\n\n~~~~~~~~ HEADERS ~~~~~~~~\n\n" + header.toString()
+                        + "\n\n~~~~~~~~         ~~~~~~~~\n");
 
         } catch (Exception e) {
             print(e);
@@ -50,149 +64,132 @@ class a {
     }
 
     public static void writeSQL() {
-        int count = 0;
-        String s;
 
-        Vector<String> data = new Vector<String>();
+        int count = 0; // counts querry number
+        String s; // stores line
+
         try (BufferedReader fin = new BufferedReader(new FileReader(csv_name))) {
             try (BufferedWriter fout = new BufferedWriter(new FileWriter(sql_name))) {
+                s = fin.readLine(); // header //and skip it cuz its header
 
-                s = fin.readLine();
-                s = fin.readLine();// to skip first line cuz its header
-
-                Iterator hi = header.iterator();
-                Boolean found = false;// if " found its set to true until another " is found in array
-                Boolean can_combine = false;
-                int start = 0;
-                int end = 0;
-                int combine_count = 0; // delete these number of elemnt at the end
-                int i;
-                Boolean action = true;
-                // Boolean action =false;
-
+                // main loop //iterates over every line
                 while (s != null) {
-                    StringBuilder sql = new StringBuilder("\n\n");
-                    String subs[] = s.split(",");
-                    Vector<String> p = new Vector<String>(Arrays.asList(subs));
-                    // StringTokenizer st = new StringTokenizer(s, ",");
-
                     count++;
-                    sql.append("/* INSERT QUERY NO: " + count + "*/\n");
-                    sql.append("INSERT INTO " + table_name + "(");
-                    for (String temp : header) {
-                        if (header.lastElement() != temp)
-                            sql.append("`"+temp + "`,");
-                        else
-                            sql.append("`"+temp + "`");
-                    }
-                    sql.append(")\nVALUES \n(");
+                    s = fin.readLine();// read nextline //skips first line cuz its header
+
+                    StringBuilder sql = new StringBuilder("\n");
+                    String subs[] = s.split(",");
+                    Vector<String> data = new Vector<String>(Arrays.asList(subs)); // convert array subs to vector for
+                                                                                   // easy handling
+                    Boolean found = false;// if " found its set to true until another " is found in array
+                    Boolean can_combine = false; // when true combines sub strings that got sepereated
+                    int start = 0; // index of where string starts
+                    int end = 0; // index of where string ends
+                    int combine_count = 0; // total number of blocks joined(delete these number of elemnt at the end)
+                    int i;
+                    Boolean lengthMismatch = false;
 
                     // print("size: " + p.size());
 
-                    for (i = 0; i < p.size(); i++) {
+                    // to join elements that is string and contains ["] :example:["hello,world"]
+                    // if we use split with [,] delemiter then ["hello] [world"] are seperated
+                    // this loop goes over the elements and join broken string into one.
+                    for (i = 0; i < data.size(); i++) {
                         can_combine = false;
-                        // char first_element=
-                        if (p.get(i).length() > 0) {
 
-                            if (p.get(i).charAt(0) == '"') {
-                                if (!found) {
-                                    found = true;
-                                    start = i;
-                                    // print("start: " + start);
-                                }
+                        if (data.get(i).length() > 0) {
+                            char firstChar = data.get(i).charAt(0);
+                            char lastChar = data.get(i).charAt(data.get(i).length() - 1);
+                            if (firstChar == '"' && !found) {
+                                found = true;
+                                start = i;
+                                // print("start:" + start);
                             }
-                            if (p.get(i).charAt(p.get(i).length() - 1) == '"') {
-                                if (found) {
-                                    found = false;
-                                    end = i;
-                                    // combine++;
-                                    can_combine = true;
-                                    // print("end: " + end);
-                                }
+                            if (lastChar == '"' && found) {
+                                found = false;
+                                can_combine = true;
+                                end = i;
+                                // print("end: " + end);
                             }
 
                             if (can_combine) {
                                 combine_count += (end - start);
+                                // join elements><
                                 for (int j = start; j < end; j++) {
-                                    // print(j);
-                                    p.set(start, p.get(start) + "," + p.get(j + 1));
-                                    // p.get(start) + "," + p[j + 1];
-                                    // print(p[start]);
+                                    data.set(start, data.get(start) + "," + data.get(j + 1));
                                 }
-                                for (int j = start + 1, k = end + 1; k < p.size(); j++, k++) {
-                                    // System.arraycopy(p, j+1, dest, destPos, length);
-                                    // int dif = end - start;
-                                    p.set(j, p.get(k));
-                                    // p[j] = p[k];
-
+                                // shift elements <--
+                                for (int j = start + 1, k = end + 1; k < data.size(); j++, k++) {
+                                    data.set(j, data.get(k));
                                 }
-
                             }
 
                         }
                     }
-
+                    // deletes the extra elements remaining after process.
                     for (int m = 0; m < combine_count; m++) {
-                        int last = p.size() - 1;
-                        p.remove(last);
+                        int last = data.size() - 1;
+                        data.remove(last);
                     }
+
                     // print("header size: "+header.size());
-                    if (p.size() != header.size()) {
-                        String err = "";
-                        err += ":::ERROR AT CSV LINE : " + (count + 1) + "\n";
-                        err += ":::::Expected no. of elements: " + header.size() + " no.of elements: " + p.size()
-                                + "\n";
-                        print(err);
+                    if (data.size() != header.size()) {
+                        // String err = "";
+                        // err += ":::ERROR AT CSV LINE : " + (count + 1) + "\n";
+                        // err += ":::::Expected no. of elements: " + header.size() + " no.of elements:
+                        // " + data.size()
+                        // + "\n";
+                        // print(err);
+
+                        if (!ignoreError) {
+                            lengthMismatch = true;
+                            print("Length mistatch! - Ignoring line " + (count + 1) + " :querry no. " + count);
+                        } else {
+                            print("Length mistatch! - line " + (count + 1) + " :querry no. " + count);
+                            print("ignoring error anyway");
+                        }
                     }
                     // print("header: " + header.size() + "\t cols: " + p.size());
+                    if (!lengthMismatch) {
+                        sql.append("/* INSERT QUERY NO: " + count + "*/\n");
+                        sql.append("INSERT INTO " + table_name + "(");
+                        for (String temp : header) {
+                            if (header.lastElement() != temp)
+                                sql.append("`" + temp + "`,");
+                            else
+                                sql.append("`" + temp + "`");
+                        }
+                        sql.append(")\nVALUES \n(");
 
-                    for (String x : p) {
-                        String temp = x;
-                        temp=temp.replace("'", "`");
-                        // print(temp);
-                        if (temp.matches("-?\\d+"))
-                            sql.append("" + temp + "");
-                        else
-                            sql.append("'" + temp + "'");
-                        if (p.lastElement() != x)
-                            sql.append(",");
-                        else
-                            sql.append("");
-                        // System.out.println(temp);
+                        for (String x : data) {
+                            String temp = x;
+                            temp = temp.replace("'", "`");
+                            // print(temp);
+                            if (temp.matches("-?\\d+"))
+                                sql.append("" + temp + "");
+                            else
+                                sql.append("'" + temp + "'");
+                            if (data.lastElement() != x)
+                                sql.append(",");
+                            else
+                                sql.append("");
+                            // System.out.println(temp);
 
+                        }
+                        sql.append("\n);");
+                        // print(sql.toString());
+                        fout.write(sql.toString());
+                        // print("count:" + combine_count);
                     }
-
-                    sql.append("\n);");
-                    // print(sql.toString());
-                    fout.write(sql.toString());
-                    // print("count:" + combine_count);
-                    s = fin.readLine();
-                    combine_count = 0;
                 }
             }
         } catch (Exception e) {
             print(e);
-            
+            e.printStackTrace();
         }
 
     }
 
-    public static void print(String args[]) {
-
-        for (int i = 0; i < args.length; i++) {
-            System.out.print(args[i]);
-        }
-    }
-
-    public static void print(String a) {
-
-        System.out.println(a);
-    }
-
-    public static void print(int a) {
-
-        System.out.println(a);
-    }
 
     public static void print(Object o) {
 
--- run.bat
+++ run.bat
@@ -4,6 +4,7 @@ del a.class
 javac a.java
 REM javac -Xlint a.java
 REM java -verbose a
-java a
+java a %1 %2 %3 
+REM java a
 
 REM code out.sql
